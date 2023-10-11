c21bafb8ba33cb2cc1997eac2b6dae91975ae974
Katy <115113751+ShidliKaty@users.noreply.github.com>
2023-10-11 13:03:28 +0300

    implement error cases in router

--- src/router.jsx
+++ src/router.jsx
@@ -1,4 +1,4 @@
-import { Navigate, createBrowserRouter } from "react-router-dom";
+import { Navigate, createBrowserRouter, useRouteError } from "react-router-dom";
 import RootLayout from "./layouts/RootLayout";
 import { postsListRoute } from "./pages/PostsList";
 import { usersListRoute } from "./pages/UsersList";
@@ -11,22 +11,44 @@ export const router = createBrowserRouter([
     path: "/",
     element: <RootLayout />,
     children: [
-      { index: true, element: <Navigate to='posts' /> },
       {
-        path: "posts",
+        errorElement: <ErrorPage />,
         children: [
-          { index: true, ...postsListRoute },
-          { path: ":postId", ...postRoute },
+          { index: true, element: <Navigate to='posts' /> },
+          {
+            path: "posts",
+            children: [
+              { index: true, ...postsListRoute },
+              { path: ":postId", ...postRoute },
+            ],
+          },
+          {
+            path: "users",
+            children: [
+              { index: true, ...usersListRoute },
+              { path: ":userId", ...userRoute },
+            ],
+          },
+          { path: "todos", ...todosListRoute },
+          { path: "*", element: <h1>404 - Page Not Found</h1> },
         ],
       },
-      {
-        path: "users",
-        children: [
-          { index: true, ...usersListRoute },
-          { path: ":userId", ...userRoute },
-        ],
-      },
-      { path: "todos", ...todosListRoute },
     ],
   },
 ]);
+
+function ErrorPage() {
+  const error = useRouteError();
+
+  return (
+    <>
+      <h1>Error - Something went wrong</h1>
+      {import.meta.env.MODE !== "production" && (
+        <>
+          <pre>{error.message}</pre>
+          <pre>{error.stack}</pre>
+        </>
+      )}
+    </>
+  );
+}
