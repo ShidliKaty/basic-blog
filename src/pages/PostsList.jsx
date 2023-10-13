9fa7239a98b61627c0dcad1a2c2d03552124934c
Katy <115113751+ShidliKaty@users.noreply.github.com>
2023-10-13 12:31:12 +0300

    Implement search query

--- src/pages/PostsList.jsx
+++ src/pages/PostsList.jsx
@@ -1,9 +1,19 @@
-import { Link, useLoaderData } from "react-router-dom";
+import { Form, Link, useLoaderData } from "react-router-dom";
 import { getPosts } from "../api/posts";
 import PostCard from "../components/PostCard";
+import { useEffect, useRef } from "react";
 
 const PostsList = () => {
-  const posts = useLoaderData();
+  const {
+    posts,
+    searchParams: { query },
+  } = useLoaderData();
+
+  const searchRef = useRef();
+
+  useEffect(() => {
+    searchRef.current.value = query;
+  }, [query]);
 
   return (
     <>
@@ -15,6 +25,22 @@ const PostsList = () => {
           </Link>
         </div>
       </h1>
+      <Form className='form mb-4'>
+        <div className='form-row'>
+          <div className='form-group'>
+            <label htmlFor='query'>Query</label>
+            <input type='search' name='query' id='query' ref={searchRef} />
+          </div>
+          {/* <div className='form-group'>
+            <label htmlFor='userId'>Author</label>
+            <select type='search' name='userId' id='userId'>
+              <option value=''>Any</option>
+              <option value='1'>Leanne Graham</option>
+            </select>
+          </div> */}
+          <button className='btn'>Filter</button>
+        </div>
+      </Form>
       <div className='card-grid'>
         {posts.map((post) => (
           <PostCard key={post.id} {...post} />
@@ -24,7 +50,14 @@ const PostsList = () => {
   );
 };
 
-const loader = ({ request: { signal } }) => getPosts({ signal });
+const loader = async ({ request: { signal, url } }) => {
+  const searchParams = new URL(url).searchParams;
+  const query = searchParams.get("query");
+  const filterParams = { q: query };
+  const posts = getPosts({ signal, params: filterParams });
+
+  return { posts: await posts, searchParams: { query } };
+};
 
 export const postsListRoute = {
   loader,
