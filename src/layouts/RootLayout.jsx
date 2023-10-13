93796951024e3a2adf268f1cc0a33ee5be1a25e6
Katy <115113751+ShidliKaty@users.noreply.github.com>
2023-10-13 15:03:11 +0300

    Refact - replace navbar

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,5 +1,5 @@
 import { Outlet, ScrollRestoration, useNavigation } from "react-router-dom";
-import NavBar from "../NavBar";
+import NavBar from "../components/NavBar";
 
 const RootLayout = () => {
   const { state } = useNavigation();
